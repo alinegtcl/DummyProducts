f5f41f1469a775d28ba5f9b6900b86375b747b32
aline <linegcruz@gmail.com>
2024-02-15 15:23:27 -0300

    feat(adapter): created adapter class to display the product list

--- app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
@@ -3,11 +3,18 @@ package br.edu.ifsp.scl.sdm.dummyproducts.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.edu.ifsp.scl.sdm.dummyproducts.R
+import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductAdapter
 import br.edu.ifsp.scl.sdm.dummyproducts.databinding.ActivityMainBinding
+import br.edu.ifsp.scl.sdm.dummyproducts.model.Product
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val productList: MutableList<Product> = mutableListOf()
+    private val productAdapter: ProductAdapter by lazy {
+        ProductAdapter(this, productList)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +23,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(amb.mainTb.apply {
             title = getString(R.string.app_name)
         })
+
+        amb.productsSp.adapter = productAdapter
     }
 }
